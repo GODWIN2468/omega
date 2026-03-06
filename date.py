729bad600f13b3f039a320df111d9f65da52a234
GODWIN2468 <Godwinasante2005@gmail.com>
2026-03-06 06:46:55 -0800

    u[dated code

---
+++ date.py
@@ -0,0 +1,21 @@
+
+#"""A simple CLI tool that prints the current date and time."""
+import datetime
+from datetime import datetime
+x = datetime.now()
+print(x)
+
+x = datetime(2026, 3, 6)
+print(x.strftime("%Y"))
+print(x.strftime("%B"))
+print(x.strftime("%d"))
+print("current date is", x.strftime("%Y-%m-%d"))
+#time
+from datetime import datetime
+now= datetime.now()
+time = now.strftime("%H: %M: %S")
+print("current time is", time)
+
+
+
+
