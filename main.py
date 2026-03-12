967e64c663e09c5fa6c4bf2d8f43afc74d7168fe
GODWIN2468 <Godwinasante2005@gmail.com>
2026-03-12 14:47:50 -0700

    new game

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
-user_Choice = int(input("what do you choose? Type 0 for Rock, 1 for paper or 2 for scissors: "))
+user_Choice = int(
+    input("what do you choose? Type 0 for Rock, 1 for paper or 2 for scissors: "))
 
 computer_Choice = random.randint(0, 2)
 print(f"computer chose {computer_Choice}")
 
 if user_Choice >= 3 or user_Choice < 0:
-    print("You typed an invalid number , you lose!") 
+    print("You typed an invalid number , you lose!")
 elif computer_Choice > user_Choice:
     print("You lose")
 elif user_Choice > computer_Choice:
